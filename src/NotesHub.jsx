154df0c998890eea5d2fe8d230f986ed1191c032
PedroDavid <u19207574@utp.edu.pe>
2023-12-30 18:59:44 -0500

    [modificando el grid de los items]

--- src/NotesHub.jsx
+++ src/NotesHub.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import {DndContext, closestCenter} from '@dnd-kit/core'
+import { listNotes } from './Scripts/listNotes'
+
 import './NotesHub.css'
+import './Scripts/listNotes'
 
 function NotesHub() {
 
@@ -9,19 +13,23 @@ function NotesHub() {
         <div className="head">
           <span className="title">
             <h2>horarios de stream</h2>
+            <span>//</span>
             <h2>Semana del 17/12/2023 al 23/12/2023</h2>
           </span>
         </div>
         <div className="body">
-          
-          <div className="grid-item">
-            <span className="notes">
-              <span className="date"><h3>Lunes</h3></span>
-              <span className="title"><h2>Detroi: Become Human Primera Vez :0</h2></span>
-              <span className="datetime"><h3>Miercoles</h3></span>
-            </span>
-          </div>
-
+          {
+            listNotes.map( ({ id, date, title, time }) => {
+              
+              return (
+                <div className="notes grid-item" key={ id }>
+                  <span className="notes__date"><h3>{ date }</h3></span>
+                  <span className="notes__title"><h2>{ title }</h2></span>
+                  <span className="notes__datetime"><h3>{ time }</h3></span>
+                </div>
+                )
+            })
+          }
         </div>
 
       </div>
---
+++ src/Scripts/listNotes.js
@@ -0,0 +1,56 @@
+export const listNotes = [
+    {
+        id: 0,
+        date: 'Lunes',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: '4 AM - Perú',
+    },
+    {
+        id: 1,
+        date: '02/03/2023',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: '4 a.m - El Salvador',
+    },
+    {
+        id: 2,
+        date: '20 Julio 2023',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: '9PM - CDMX',
+    },
+    {
+        id: 3,
+        date: '02-03-2023',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: 'Diciembre 07',
+    },
+    {
+        id: 4,
+        date: 'Viernes-4',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: 'Diciembre 07',
+    },
+    {
+        id: 5,
+        date: 'Viernes-5',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: 'Diciembre 07',
+    },
+    {
+        id: 6,
+        date: 'Viernes-6',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: 'Diciembre 07',
+    },
+    {
+        id: 7,
+        date: 'Viernes-7',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: 'Diciembre 07',
+    },
+    {
+        id: 8,
+        date: 'Viernes-8',
+        title: 'Detroi: Become Human Primera Vez :0',
+        time: 'Diciembre 07',
+    },
+]
