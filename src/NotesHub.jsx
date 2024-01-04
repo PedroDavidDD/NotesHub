60dcaba3c488dcaa2ea5c922339a356f0bbbae2a
PedroDavid <u19207574@utp.edu.pe>
2024-01-03 20:13:18 -0500

    [upd-responsive]
    + arreglado

--- src/NotesHub.jsx
+++ src/NotesHub.jsx
@@ -9,27 +9,36 @@ function NotesHub() {
 
   return (
     <>
-      <div className="container">
-        <div className="head">
-          <span className="title">
-            <h2>horarios de stream</h2>
-            <span>//</span>
-            <h2>Semana del 17/12/2023 al 23/12/2023</h2>
-          </span>
-        </div>
-        <div className="body">
-          {
-            listNotes.map( ({ id, date, title, time }) => {
-              
-              return (
-                <div className="notes grid-item" key={ id }>
-                  <span className="notes__date"><h3>{ date }</h3></span>
-                  <span className="notes__title"><h2>{ title }</h2></span>
-                  <span className="notes__datetime"><h3>{ time }</h3></span>
-                </div>
-                )
-            })
-          }
+      <div className="background">
+
+
+        <div className="box">
+
+          <div className="box__calendar"> 
+
+            <span className="calendar__title">
+              <h2>horarios de stream</h2>
+              <span>//</span>
+              <h2>Semana del 17/12/2023 al 23/12/2023</h2>
+            </span>
+
+            <div className="calendar__notes">
+              {
+                listNotes.map( ({ id, date, title, time }) => {
+                  
+                  return (
+                    <div className="note grid-item" key={ id }>
+                      <span className="note__date"><h3>{ date }</h3></span>
+                      <span className="note__title"><h2>{ title }</h2></span>
+                      <span className="note__datetime"><h3>{ time }</h3></span>
+                    </div>
+                    )
+                })
+              }
+            </div>
+            
+          </div>
+
         </div>
 
       </div>
