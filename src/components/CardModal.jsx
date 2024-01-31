00cccea93f8a52055e1692493f853cbc2726b60e
AvariciaT <83774625+PedroDavidDD@users.noreply.github.com>
2024-01-30 21:28:42 -0500

    [UPD-BoxModel]
    - CON MATERIAL UI -
    - Falta igualar estilo de Box con la de mayor detalle.

--- src/components/CardModal.jsx
+++ src/components/CardModal.jsx
@@ -1,5 +1,6 @@
 import { Box, Modal, Typography } from '@mui/material'
 import React from 'react'
+import { theme } from '../css/theme';
 
     const style = {
         position: 'absolute',
@@ -8,40 +9,46 @@ import React from 'react'
         transform: 'translate(-50%, -50%)',
         with: 500,
         height: 500,
-        bgcolor: '#FE5CFF',
-        bgcolor: '#222',
+        // bgcolor: '#FE5CFF',
+        // bgcolor: '#222',
         border: '2px solid #000',
         boxShadow: 24,
         p: 4,
         display: 'flex',
         flexDirection: 'column',
         alignContext: 'center',
-        justifyContent: 'space-between',
-        
+        justifyContent: 'space-around',
+
+        background: `#E5E2D1 url('./images/00004.jpeg') no-repeat center/cover`,        
+        borderRadius: "10px 10px",
+        filter: "saturate(120%)",
     };
 
     const cardDate = {
         display: 'flex',
         justifyContent: 'flex-start',
-        color: '#fff',
+        color: theme.colors.common.black,      
     };
+    
     const cardTitle = {
         display: "block",
-        textAlign: 'center',
-        color: '#000',
-        bgcolor: '#fff',
+        // textAlign: 'center',
+        color: theme.colors.common.black,  
     };
+
     const cardContet = {
         width: '450px',
         height: '350px',
         textAlign: 'justify',
-        color: '#fff',
         px: 2,
         overflow: 'auto',
+        color: theme.colors.common.black, 
     };
+
     const cardTime = {
         display: 'flex',
         justifyContent: 'flex-end',
+        color: theme.colors.common.black, 
     };
   
 export const CardModal =({ open, handleClose, id, date, title, time, content, stateNotes })=> {
@@ -54,19 +61,20 @@ export const CardModal =({ open, handleClose, id, date, title, time, content, st
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
+            
             >
-            <Box 
-                sx={style}
-                className={`note grid-item ${stateNotes}`} 
-                key={ id } 
-            >
-                <Typography variant="h6" component="div" sx={cardDate} >{ date }</Typography>
-                <span>
-                    <Typography  variant="h5" component="div" sx={cardTitle} >{ title }</Typography>
-                    <Typography paragraph variant="h6" sx={cardContet} >{ content }</Typography>
-                </span>
-                <Typography variant="h6" component="div" sx={cardTime} >{ time }</Typography>
-            </Box>
+                <Box 
+                    sx={style}
+                    className={`note grid-item`} 
+                    key={ id } 
+                >
+                    <Typography className={`note__date`} variant="h6" component="div" sx={cardDate} >{ date }</Typography>
+                    <span>
+                        <Typography className={`note__title`} variant="h5" component="div" sx={cardTitle} >{ title }</Typography>
+                        <Typography paragraph variant="h6" sx={cardContet} >{ content }</Typography>
+                    </span>
+                    <Typography className={`note__datetime`} variant="h6" component="div" sx={cardTime} >{ time }</Typography>                    
+                </Box>
             </Modal>
         </>
     )
---
+++ src/css/theme/index.js
@@ -0,0 +1,43 @@
+export const theme = {
+  colors: {
+    border: "#0075FF",
+    error: "#bb2929",
+    success: "#1ed12d",
+    info: "#0791e6",
+    alert: "#FFCF00",
+    common: {
+      black: "#000",
+      white: "#fff",
+    },
+    bg: {
+      body: "#C1c5c5",
+    },
+    primary: {
+      main: "#4D2BAA",
+      hover: "#3E18A7",
+      contrast: "#000",
+    },
+    grey: {
+      50: "#fafafa",
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  size: {
+    card: {
+      width: "310px",
+      height: "310px",
+    },
+    modal: {
+      width: "310px",
+      height: "310px",
+    },
+  },
+};
