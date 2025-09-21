b4c53d9eabd7e7dcb7b74278bee4f1b3ede8a73e
PedroDavidDD <avariciatz@gmail.com>
2025-09-21 00:55:49 -0500

    refactor(ButtonClose): set button type

--- src/components/ButtonClose.tsx
+++ src/components/ButtonClose.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { theme } from '../css/theme'
-import { X } from 'lucide-react'
+import React from "react";
+import { theme } from "../css/theme";
+import { X } from "lucide-react";
 
-export const ButtonClose = ({ onClose }) => {
+export const ButtonClose = ({ onClose }: { onClose: () => void }) => {
   return (
     <button
-        onClick={onClose}
-        className={`absolute top-6 right-6 p-2 rounded-full transition-colors text-[${theme.colors.common.white}] hover:border-white`}
-        style={{
-        backgroundColor: theme.navbar.background
-        }}
+      onClick={onClose}
+      className={`absolute top-6 right-6 p-2 rounded-full transition-colors text-[${theme.colors.common.white}] hover:border-white`}
+      style={{
+        backgroundColor: theme.navbar.background,
+      }}
     >
-        <X size={20} />
+      <X size={20} />
     </button>
-  )
-}
+  );
+};
